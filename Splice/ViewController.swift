86faabc41432f683a9e3be2b384e771d297c3f9d
June <kimjune01@gmail.com>
2021-10-24 14:24:04 -0700

    Initial Commit

---
+++ Splice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Splice
+//
+//  Created by June Kim on 10/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
